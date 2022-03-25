ecbc290ae305c14b559e14fc42810f0b1f59026e
cannian09 <76438513+cannian09@users.noreply.github.com>
2022-03-25 19:59:53 +0800

    Update Triangle.h

--- C++ w5/ex2/Triangle.h
+++ C++ w5/ex2/Triangle.h
@@ -1,1 +1,20 @@
+#pragma once
+class Triangle
+{
+public:
+	enum Type {
+		Equilateral,
+		Iososceles,
+		Right,
+		Normal,
+		Invalid
+	};
+
+	Triangle(float length1, float length2, float length3);
+	Type GetType();
+	float* GetSideLength();
+private:
+	float side_length[3];
+};
+
 
