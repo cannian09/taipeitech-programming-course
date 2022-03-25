46f0e4022316f78c18a1e0c51c60ee70ace4979a
cannian09 <76438513+cannian09@users.noreply.github.com>
2022-03-25 19:59:27 +0800

    Update Triangle.cpp

--- C++ w5/ex2/Triangle.cpp
+++ C++ w5/ex2/Triangle.cpp
@@ -1,1 +1,33 @@
+#include "Triangle.h"
 
+Triangle::Triangle(float length1, float length2, float length3)
+{
+	side_length[0] = length1;
+	side_length[1] = length2;
+	side_length[2] = length3;
+}
+
+Triangle::Type Triangle::GetType()
+{
+	if (side_length[0] == side_length[1] && side_length[2]==side_length[1]) {
+		return Equilateral;
+	}
+	if (side_length[0] == side_length[1] || side_length[1] == side_length[2] || side_length[0] == side_length[2]) {
+		return Iososceles;
+	}
+	if (((side_length[0] * side_length[0]) + (side_length[1] * side_length[1]) == side_length[2] * side_length[2]) || ((side_length[1] * side_length[1]) + (side_length[2] * side_length[2]) == side_length[0] * side_length[0]) || ((side_length[0] * side_length[0]) + (side_length[2] * side_length[2]) == side_length[1] * side_length[1])) {
+		return Right;
+	}
+	
+	if ((side_length[0] + side_length[1]) < side_length[2] || (side_length[1] + side_length[2]) < side_length[0] || (side_length[1] + side_length[3] < side_length[2])) {
+		return Invalid;
+	}
+	else {
+		return Normal;
+	}
+}
+
+float* Triangle::GetSideLength()
+{
+	return side_length;
+}
